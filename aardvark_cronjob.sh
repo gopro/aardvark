c0ab986e0dbcdb92d6f93fc46aa29ac6ca15690e
Mihai Baciu <mbaciu@gopro.com>
2019-10-15 16:47:47 +0300

    SSDO-0000 python fullpath

--- aardvark_cronjob.sh
+++ aardvark_cronjob.sh
@@ -3,7 +3,7 @@ export AARDVARK_ACCOUNTS
 export DATADOG_SECRET
 
 
-python /tmp/aardvark/dd_event.py start
+/usr/local/bin/python /tmp/aardvark/dd_event.py start
 # redirect to stdout of pid 1, which in our case (docker) will cause the output to be visible for docker logs
 /usr/local/bin/aardvark update -a ${AARDVARK_ACCOUNTS} >> /proc/1/fd/1
-python /tmp/aardvark/dd_event.py end
+/usr/local/bin/python /tmp/aardvark/dd_event.py end
